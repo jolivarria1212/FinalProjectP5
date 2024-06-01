eecdb188f23b47fc9902dd52737edbf8a0ab6353
74309 <743095@student.dusd.net>
2024-05-31 19:13:40 -0700

    Updated All scripts to work
    Made all the scripts work Stopped video at 1:39:47

--- P5ProjectFinal/Assets/Scripts/Player.cs
+++ P5ProjectFinal/Assets/Scripts/Player.cs
@@ -6,7 +6,6 @@ public class Player : MonoBehaviour
 {
     private Animator animator;
     private bool isHopping;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -18,15 +17,28 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.W) && !isHopping)
         {
-            animator.SetTrigger("Hop");
-            isHopping = true;
-            int zDifference = 0;
+            float zDifference = 0;
             if (transform.position.z % 1 != 0)
             {
-                zDifference = transform.position.z  Mathf.Round(transform.position.z);
+                zDifference = Mathf.Round(transform.position.z) - transform.position.z;
             }
-            transform.position = (transform.position + new Vector3(1, 0, zDifference));
+            MoveCharacter(new Vector3(1, 0, zDifference));
         }
+        else if (Input.GetKeyDown(KeyCode.A) && !isHopping)
+        {
+            MoveCharacter(new Vector3(0, 0, 1));
+        }
+        else if (Input.GetKeyDown(KeyCode.D) && !isHopping)
+        {
+            MoveCharacter(new Vector3(0, 0, -1));
+        }
+    }
+    
+    private void MoveCharacter(Vector3 difference)
+    {
+        animator.SetTrigger("Hop");
+        isHopping = true;
+        transform.position = (transform.position + difference);
     }
 
     public void FinishHop()
